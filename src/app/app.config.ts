a44b571edc4f4eb3b084de484e4faf0ea0219882
Mohit Choudhary <mohitchaudhary3523@gmail.com>
2025-11-07 23:28:35 +0530

    Fix: added zone.js polyfill and updated tsconfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
 import { ApplicationConfig, APP_INITIALIZER, inject } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import {routes } from './app.routes';
+import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { DatabaseService } from './core/database';
 
-// ensure DB is initialized before app starts
 function initDbFactory() {
   const db = inject(DatabaseService);
   return () => db.init();
@@ -13,7 +13,8 @@ function initDbFactory() {
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(),         
+    provideAnimations(),        
     DatabaseService,
     { provide: APP_INITIALIZER, useFactory: initDbFactory, multi: true }
   ],
--- src/app/services/weather.ts
+++ src/app/services/weather.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class WeatherService {
 
-  private apiKey = 'fd772a5a03cc6fee48852264d3a883efE';
+  private apiKey = 'DUMMY_API_KEY';
   private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
   constructor(private http: HttpClient) {}
